72a3602907709ae2679e5202b20d692e21d202ed
Hanie24 <sailor_121108@hotmail.com>
2018-04-05 12:07:54 -0500

    datos

---
+++ js/data.js
@@ -0,0 +1,37 @@
+const data = {
+    desayuno: [
+        { item: 'Cafe americano', price: '5'},
+        { item: 'Cafe con leche ', price: '7'},
+        { item: 'Sandwich de jamón y queso', price: '10'},
+        { item: 'Jugo natural', price: '7'}
+    ],
+    
+    acompañamientos: [
+        { item: 'Papas fritas', price: '5'},
+        { item: 'Onion Rings', price: '5'},
+    ],
+    
+    hamburguesas: [
+        { item: 'res', sencilla: '10', doble: '15'},
+        { item: 'pollo', sencilla: '10', doble: '15'},
+        { item: 'vegetariana', sencilla: '10', doble: '15'},
+    ],
+    
+    bebidas: [
+        { item: 'agua', size:{
+            grande: '8',
+            chico: '5'
+        }},
+        { item: 'gaseosa', size:{
+            grande: '10',
+            chico: '5'
+        }}
+    ],
+    
+    ingredienteExtra: [
+        { item: 'huevo', price: '5'},
+        { item: 'queso', price: '5'},
+    ]
+}
+
+export default data
