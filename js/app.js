b831a875bfdeaa2689f83b8045de2e45a85ef1d5
Hanie24 <sailor_121108@hotmail.com>
2018-04-06 13:01:13 -0500

    borrar Readmy

--- js/app.js
+++ js/app.js
@@ -1,3 +1,20 @@
 import React, {Component} from 'react';
-import ReactDom from 'react-dom';
+import ReactDOM from 'react-dom';
+
+class Button extends React.Component{
+    render(){
+        return (
+            <div>
+                <h1>Bienvenidos a Burger Queen</h1>
+                <button>Comenzar</button>
+            </div>
+        );
+    };
+};
+
+//Renderizar componente 
+const rootElement = document.getElementById('react-container');
+const element = <Button/>;
+
+ReactDOM.render(element, rootElement);
 
